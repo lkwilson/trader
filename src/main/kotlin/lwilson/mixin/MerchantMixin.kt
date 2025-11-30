a073a1e39f912dfdb6c12f6a5fe06740660f4cac
Lucas Wilson <lkwilson96@gmail.com>
2025-11-30 08:25:48 -0700

    is server side the wrong one?

--- src/main/kotlin/lwilson/mixin/MerchantMixin.kt
+++ src/main/kotlin/lwilson/mixin/MerchantMixin.kt
@@ -2,11 +2,13 @@ package lwilson.mixin
 
 import net.minecraft.entity.passive.VillagerEntity
 import net.minecraft.entity.passive.MerchantEntity
+import net.minecraft.text.Text
 import org.slf4j.LoggerFactory
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.injection.At
 import org.spongepowered.asm.mixin.injection.Inject
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import net.minecraft.client.MinecraftClient
 
 @Mixin(MerchantEntity::class)
 abstract class MerchantMixin {
@@ -16,19 +18,14 @@ abstract class MerchantMixin {
   private fun onFillRecipesFromPool(info: CallbackInfo) {
     logger.info("fillRecipesFromPool")
     val merchant = this as MerchantEntity
-    // if (merchant is VillagerEntity) {
-    //     val world = merchant.world ?: return
-    //     world.players.forEach { player ->
-    //         player.sendMessage(Text.literal("Villager updated!"), true)
-    //     }
-    // }
-
-    // val client = MinecraftClient.getInstance()
-    // val player = client.player
-    // player?.sendMessage(Text.literal("Message"), false)
-    logger.info("Villager trades updated!")
+    if (!(merchant is VillagerEntity)) return
+    val msg = Text.literal("Villager updated:");
     for (x in merchant.offers) {
-      logger.info("Offer: ${x.sellItem} ${x.sellItem.enchantments.toString()}")
+      msg.append("\n  - ${x.sellItem}")
     }
+
+    val client = MinecraftClient.getInstance()
+    val player = client.player ?: return
+    player.sendMessage(msg, false);
   }
 }
